67f0e60b150e71e0f8261f07180129f78f4fb9e4
drnisar <drnisar@hotmail.com>
2024-12-21 18:30:44 +0500

    Build the NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,31 @@
+import Link from "next/link";
 import React from "react";
+import { IoBug } from "react-icons/io5";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const links = [
+    { href: "/", label: "Dashboard" },
+    { href: "/issues", label: "Issues" },
+  ];
+  return (
+    <nav className="flex h-14 border-b px-6 space-x-6 items-center">
+      <Link href="/">
+        <IoBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              href={link.href}
+              className="text-zinc-600 hover:text-zinc-800 transition-colors"
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default NavBar;
